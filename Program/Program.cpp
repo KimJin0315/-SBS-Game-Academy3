37f691a50921f2d5a1e76b3c5ed6049a25dabddb
KimJin0315 <xnxl159@naver.com>
2025-01-31 19:44:08 +0900

    (2025.01.31) 템플릿

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,49 +2,30 @@
 
 using namespace std;
 
-class Vector2
+template <typename T>
+class List 
 {
 private:
-    int x;
-    int y;
+    int size;
+    int index;
+
+    T * pointer;
 public:
-    Vector2(int x, int y)
-    {
-        this->x = x;
-        this->y = y;
-    }
-
-    const int & X() { return x; }
-
-    const int& Y() { return y; }
-
-    Vector2 operator + (const Vector2 & clone)
-    {
-        Vector2 newVector(this->x + clone.x, this->y + clone.y);
-
-        return newVector;
-    }
     
-    void ShowVector()
-    {
-        cout << "x의 값 : " << x << ", " << "y의 값 : " << y << endl;
-    }
 
 };
 
 int main()
 {
-#pragma region 연산자 오버로딩
-    
-    Vector2 direction1(1, 1);
-    Vector2 direction2(5, 5);
+#pragma region 템플릿
+    // 데이터 형식에 의존하지 않고, 하나의 값이 여러 다른 데이터
+    // 형식을 가질 수 있는 기술에 중점을 두어 재사용성을 높일 수
+    // 있는 기능입니다.
 
-    Vector2 direction3 = direction1 + direction2;
-
-    direction3.ShowVector();
-   
+    List<int> intList;
 
 #pragma endregion
+
 
 
     return 0;
