d9b44b20a446a48fdf4c2276daaa15a8cedad46b
KimJin0315 <xnxl159@naver.com>
2025-02-10 19:44:35 +0900

    (2025.02.10) 연관 컨테이너

---
+++ Class14th (Container Adapter)/Source.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+#pragma region Stack
+
+    //  std::stack<int> stack;
+    // 
+    //  stack.push(10);
+    //  stack.push(20);
+    //  stack.push(30);
+    // 
+    //  cout << "Stack Size : " << stack.size() << endl;
+    // 
+    //  while (!stack.empty())
+    //  {
+    //      cout << stack.top() << endl;
+    // 
+    //      stack.pop();
+    //  }
+
+#pragma endregion
+
+#pragma region queue
+    // std::queue<int> queue;
+
+    // queue.push(10);
+    // queue.push(20);
+    // queue.push(30);
+    // queue.push(40);
+    // queue.push(50);
+    // 
+    // while (queue.empty() == false)
+    // {
+    //     cout << queue.front() << " ";
+    //     
+    //     queue.pop();
+    // }
+
+#pragma endregion
+
+    return 0;
+
+}
